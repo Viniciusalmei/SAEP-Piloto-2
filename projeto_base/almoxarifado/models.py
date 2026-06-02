197a04afa12ddf444e0b12e4c4ba36d406213ba9
juhreisss <juhreis2508@gmail.com>
2026-06-02 08:08:53 -0300

    Subindo alterações models sobre user

--- projeto_base/almoxarifado/models.py
+++ projeto_base/almoxarifado/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('operador', 'Operador de Almoxarifado'),
+        ('administrador', 'Administrador do Sistema'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    tipo = models.CharField(max_length=20, choices=ROLE_CHOICES, default='operador')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_tipo_display()}"
+
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
