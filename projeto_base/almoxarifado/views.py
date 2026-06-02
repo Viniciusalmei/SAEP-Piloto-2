6036f947a517f32c947459e3f3806f44d262bb5c
JoaoMafra07 <jrlorimafra@gmail.com>
2026-06-02 09:51:14 -0300

    feat: adiciona permission classes para permissionamento

--- projeto_base/almoxarifado/views.py
+++ projeto_base/almoxarifado/views.py
@@ -1,11 +1,20 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, serializers
+from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Produto, Movimentacao
 from .serializers import ProdutoSerializer, MovimentacaoSerializer
+from .permissions import RoleBasedAccessPermission
+
+class ProdutoPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+    permission_classes = [RoleBasedAccessPermission]
+    pagination_class = ProdutoPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['nome', 'data_cadastro']
     search_fields = ['nome']
@@ -14,6 +23,7 @@ class ProdutoViewSet(viewsets.ModelViewSet):
 class MovimentacaoViewSet(viewsets.ModelViewSet):
     queryset = Movimentacao.objects.all()
     serializer_class = MovimentacaoSerializer
+    permission_classes = [RoleBasedAccessPermission]
     
     def perform_create(self, serializer):
         # ATENÇÃO ALUNO! Implemente a logica de estoque aqui 
