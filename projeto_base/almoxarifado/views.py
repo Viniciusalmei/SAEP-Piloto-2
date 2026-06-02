3ce7222a37c4d1bdac28fb66b8fb2dfa6fb8af61
rayane1550 <rayrayvguimas26@gmail.com>
2026-06-02 09:34:08 -0300

    adicionando views

--- projeto_base/almoxarifado/views.py
+++ projeto_base/almoxarifado/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Produto, Movimentacao
 from .serializers import ProdutoSerializer, MovimentacaoSerializer
@@ -16,5 +17,25 @@ class MovimentacaoViewSet(viewsets.ModelViewSet):
     serializer_class = MovimentacaoSerializer
     
     def perform_create(self, serializer):
-        # ATENÇÃO ALUNO! Implemente a logica de estoque aqui 
+        produto = serializer.validated_data['produto']
+        quantidade = serializer.validated_data['quantidade']
+        tipo = serializer.validated_data['tipo']
+
+        if quantidade <= 0:
+            raise ValidationError({"quantidade": "A quantidade informada deve ser maior que zero."})
+
+        if tipo == 'E':
+        
+            produto.quantidade_estoque += quantidade
+        elif tipo == 'S':
+        
+            if produto.quantidade_estoque < quantidade:
+                raise ValidationError({"quantidade": f"Estoque insuficiente. O saldo atual é de {produto.quantidade_estoque} unidades."})
+      
+            produto.quantidade_estoque -= quantidade
+        
+
+        produto.save()
+        
+       
         serializer.save(usuario=self.request.user)
