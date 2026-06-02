3ac3d78d36945836335c3d6be52f7fba8de34ff8
ThomasTonho <antonellithomasv@gmail.com>
2026-06-02 08:25:11 -0300

    Instalação do JWT e atualização do requirements

---
+++ almoxarifado/views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Produto, Movimentacao
+from .serializers import ProdutoSerializer, MovimentacaoSerializer
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['nome', 'data_cadastro']
+    search_fields = ['nome']
+    ordering_fields = ['nome', 'data_cadastro']
+
+class MovimentacaoViewSet(viewsets.ModelViewSet):
+    queryset = Movimentacao.objects.all()
+    serializer_class = MovimentacaoSerializer
+    
+    def perform_create(self, serializer):
+        # ATENÇÃO ALUNO! Implemente a logica de estoque aqui 
+        serializer.save(usuario=self.request.user)
