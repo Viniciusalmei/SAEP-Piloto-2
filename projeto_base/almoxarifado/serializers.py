e102746c9944ca090d33805bb15563032abe7f6f
Nicolasvictor07 <Nicolasvictor0402@gmail.com>
2026-06-02 08:41:50 -0300

    Serializers

--- projeto_base/almoxarifado/serializers.py
+++ projeto_base/almoxarifado/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from .models import Produto, Movimentacao
+from .models import Produto, Movimentacao, Perfil
 from django.contrib.auth.models import User
 
+class PerfilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Perfil
+        fields = ['tipo']
+
 class UserSerializer(serializers.ModelSerializer):
+   
+    perfil = PerfilSerializer(read_only=True)
+    
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'perfil']
 
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +28,32 @@ class MovimentacaoSerializer(serializers.ModelSerializer):
         model = Movimentacao
         fields = '__all__'
         read_only_fields = ['usuario', 'data_hora']
+
+    def validate(self, data):
+        tipo = data.get('tipo')
+        quantidade = data.get('quantidade')
+        produto = data.get('produto')
+
+        # Bloqueio de operação e Mensagem erro
+        if tipo == 'S' and quantidade > produto.quantidade_estoque:
+            mensagem = f"Saída não permitida: estoque insuficiente. Disponível {produto.quantidade_estoque}: Solicitado:{quantidade}"
+            raise serializers.ValidationError(mensagem)
+        
+        return data
+
+    def create(self, validated_data):
+        tipo = validated_data.get('tipo')
+        quantidade = validated_data.get('quantidade')
+        produto = validated_data.get('produto')
+
+        # Lógica de atualização do estoque
+        if tipo == 'E':
+            produto.quantidade_estoque += quantidade
+        elif tipo == 'S':
+           
+            produto.quantidade_estoque -= quantidade
+        
+        produto.save()
+
+        
+        return super().create(validated_data)
