45ab4cadc8fca788c2b1668911fb2e41b648ffe4
JoaoMafra07 <jrlorimafra@gmail.com>
2026-06-02 10:18:52 -0300

    feat: formatação do serializer de movimentacoes

--- projeto_base/almoxarifado/serializers.py
+++ projeto_base/almoxarifado/serializers.py
@@ -15,8 +15,24 @@ class ProdutoSerializer(serializers.ModelSerializer):
 class MovimentacaoSerializer(serializers.ModelSerializer):
     usuario_nome = serializers.ReadOnlyField(source='usuario.username')
     produto_nome = serializers.ReadOnlyField(source='produto.nome')
+    data_hora = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S", read_only=True)
+    
+    estoque_anterior = serializers.SerializerMethodField()
+    estoque_atual = serializers.SerializerMethodField()
 
     class Meta:
         model = Movimentacao
-        fields = '__all__'
+        fields = ['id', 'usuario', 'usuario_nome', 'produto', 'produto_nome',
+                  'tipo', 'quantidade', 'estoque_anterior', 'estoque_atual',
+                  'data_hora']
         read_only_fields = ['usuario', 'data_hora']
+
+    def get_estoque_anterior(self, obj):
+        if obj.tipo == 'E':
+            return obj.produto.quantidade_estoque - obj.quantidade
+        elif obj.tipo == 'S':
+            return obj.produto.quantidade_estoque + obj.quantidade
+        return obj.produto.quantidade_estoque
+
+    def get_estoque_atual(self, obj):
+        return obj.produto.quantidade_estoque
